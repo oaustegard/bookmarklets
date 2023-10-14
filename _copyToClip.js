716857d9ce89f919575a81039a26f6f0820270e0
Oskar Austegard <oskar@austegard.com>
2023-10-14 14:44:48 -0400

    Initial commit

---
+++ _copyToClip.js
@@ -0,0 +1,11 @@
+/* utility function to copy text to clipboard, uses the older execCommand as it's more reliable */
+function copyToClip(doc, html, text = null) {
+    function listener(e) {
+        e.clipboardData.setData("text/html", html);
+        e.clipboardData.setData("text/plain", text || html);
+        e.preventDefault();
+    }
+    doc.addEventListener("copy", listener);
+    doc.execCommand("copy");
+    doc.removeEventListener("copy", listener);
+}
