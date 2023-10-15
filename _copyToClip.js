93f81a7c4db8580f3f275154343c226d22353740
Oskar Austegard <oskar@austegard.com>
2023-10-14 22:27:19 -0400

    adding second batch

--- _copyToClip.js
+++ _copyToClip.js
@@ -1,4 +1,4 @@
-/* utility function to copy text to clipboard, uses the older execCommand as it's more reliable */
+/* utility function to copy text to clipboard, uses the older execCommand as it's more reliable -- copies both formatted and plaintext*/
 function copyToClip(doc, html, text = null) {
     function listener(e) {
         e.clipboardData.setData("text/html", html);
