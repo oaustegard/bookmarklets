7ba884f643335d4f9c85a16e7045d07feae99cf7
Oskar Austegard <oskar@austegard.com>
2025-02-15 22:17:03 -0500

    Update arxiv_abstract.js
    Made it bidirectional

--- arxiv_abstract.js
+++ arxiv_abstract.js
@@ -1,9 +1,20 @@
-javascript: (function() {
-	if (window.location.hostname !== 'arxiv.org') {
-		alert('This bookmarklet only works on pdfs on arxiv.org');
-		return;
-	}
-	var currentURL = window.location.href;
-	var newURL = currentURL.replace("/pdf/", "/abs/").replace(".pdf", "");
-	window.open(newURL, '_blank');
+javascript:(function() {
+    /* Toggles between arXiv PDF and abstract views, working in both directions */
+    if (window.location.hostname !== 'arxiv.org') {
+        alert('This bookmarklet only works on arxiv.org');
+        return;
+    }
+    var currentURL = window.location.href;
+    var newURL;
+    if (currentURL.includes('/pdf/')) {
+        newURL = currentURL.replace('/pdf/', '/abs/').replace('.pdf', '');
+    } 
+    else if (currentURL.includes('/abs/')) {
+        newURL = currentURL.replace('/abs/', '/pdf/') + '.pdf';
+    }
+    else {
+        alert('This bookmarklet only works on arXiv abstract or PDF pages');
+        return;
+    }
+    window.open(newURL, '_blank');
 })();
