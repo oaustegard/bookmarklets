ac1ce4843e38d6c8ee109cf2a92237826a84606f
Oskar Austegard <oskar@austegard.com>
2025-12-04 14:03:33 -0500

    Update arxiv_abstract.js

--- arxiv_abstract.js
+++ arxiv_abstract.js
@@ -3,7 +3,6 @@ javascript:
 /* @description: Toggles between arXiv PDF and abstract views, working in both directions */
 /* @domains: arxiv.org */
 (function() {
-    /* 
     if (window.location.hostname !== 'arxiv.org') {
         alert('This bookmarklet only works on arxiv.org');
         return;
